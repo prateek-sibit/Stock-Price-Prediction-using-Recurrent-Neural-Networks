4c53936d6d15078a69244baf7eb03436e68bf263
prateek-sibit <prateeksoni2301@gmail.com>
2018-06-21 14:40:14 +0530

    Building the RNN

--- rnn.py
+++ rnn.py
@@ -39,9 +39,41 @@ X_train = np.reshape(X_train, newshape=(X_train.shape[0],X_train.shape[1],1))
 
 
 
-
-pd.DataFrame.drop()
-
 # Part 2 - Building the RNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+# Initialising the RNN
+regressor = Sequential()
+
+# Adding the first LSTM Layer and some dropout regularization
+regressor.add(LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1],1)))
+regressor.add(Dropout(0.2))
+
+# Adding a second LSTM Layer and some dropout regularization
+regressor.add(LSTM(units=50,return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding a third LSTM Layer and some dropout regularization
+regressor.add(LSTM(units=50,return_sequences=True))
+regressor.add(Dropout(0.2))
+
+# Adding a fourth LSTM Layer and some dropout regularization
+regressor.add(LSTM(units=50))
+regressor.add(Dropout(0.2))
+
+# Adding the output layer
+regressor.add(Dense(units=1))
+
+# Compiling the RNN
+regressor.compile(optimizer='adam',loss='mean_squared_error')
+
+# Fitting the RNN to the Training set
+regressor.fit(X_train,y_train,batch_size=32,epochs=100)
+
 
 # Part 3 - Making the predictions and visualising the results
